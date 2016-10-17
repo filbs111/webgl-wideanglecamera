b53964414b0021d02c6d7940123bc118f1da9fde
filbs111 <ed.filby@gmail.com>
2016-10-17 02:24:43 +0100

    #8 vertex displacement method initial submit
    basically works for strength=1. TODO add strength dependence
    includes enabling backface culling

--- data/levelCubeData.js
+++ data/levelCubeData.js
@@ -83,4 +83,76 @@ var levelCubeData={
       16, 17, 18,   16, 18, 19, // Right face
       20, 21, 22,   20, 22, 23  // Left face
     ]
-}
+};
+
+var subdivideMeshData = function(meshData){
+	//keep things simple - just subdivide each triangle into 4 triangles.
+	//can call repeatedly for finer subdivision
+	
+	var indices = meshData.indices;	//note this is the same data - 
+	var vertices = meshData.vertices;
+	var uvcoords = meshData.uvcoords;
+	
+	var nextVert = vertices.length;
+	var nextUv = uvcoords.length;
+	var nextNum = nextUv/2;	//note assuming that nextVert = nextNum*3
+	
+	var numindices = indices.length;
+	var pairs={};
+
+	var newindices=[];
+	var newidx=0;	//could use newindices.length
+	//loop over indices. for each triangle, add an entry, if it doesn't already exist, to the list of pairs
+	
+	var numTris = indices.length/3;	//TODO check is a multiple of 3
+	
+	for (var ii=0;ii<numindices;ii+=3){
+		
+		var idxA = indices[ii];
+		var idxB = indices[ii+1];
+		var idxC = indices[ii+2];
+		
+		addPairs(idxA,idxB);
+		addPairs(idxB,idxC);
+		addPairs(idxC,idxA);
+		
+		function addPairs(idxP,idxQ){
+			if (!pairs[[idxP,idxQ]]){
+				pairs[[idxP,idxQ]]=nextNum;
+				pairs[[idxQ,idxP]]=nextNum++;
+				
+				//set vertex position and texture coordinate as average of pair
+
+				vertices[nextVert++]= 0.5*(vertices[3*idxP] + vertices[3*idxQ]);
+				uvcoords[nextUv++]= 0.5*(uvcoords[2*idxP] + uvcoords[2*idxQ]);
+
+				vertices[nextVert++]= 0.5*(vertices[3*idxP +1] + vertices[3*idxQ +1]);
+				uvcoords[nextUv++]= 0.5*(uvcoords[2*idxP +1] + uvcoords[2*idxQ +1]);
+
+				vertices[nextVert++]= 0.5*(vertices[3*idxP +2] + vertices[3*idxQ +2]);
+			}
+		}
+		
+		//make new triangles
+		
+		newindices[newidx++] = idxA;
+		newindices[newidx++] = pairs[[idxA,idxB]];
+		newindices[newidx++] = pairs[[idxA,idxC]];
+		
+		newindices[newidx++] = pairs[[idxA,idxB]];
+		newindices[newidx++] = idxB;
+		newindices[newidx++] = pairs[[idxB,idxC]];
+		
+		newindices[newidx++] = pairs[[idxA,idxC]];
+		newindices[newidx++] = pairs[[idxB,idxC]];
+		newindices[newidx++] = idxC;
+		
+		newindices[newidx++] = pairs[[idxA,idxB]];
+		newindices[newidx++] = pairs[[idxB,idxC]];
+		newindices[newidx++] = pairs[[idxA,idxC]];
+		
+	}
+	
+	//swap out index data - note this changes the original object...
+	meshData.indices = newindices;
+};
